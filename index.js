db6a2abebc837151da03c3af7544760afc69797e
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-30 03:47:33 +0530

    Making it production ready with securing the header response and adding request logging via morgan

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const courseRoutes = require('./routes/course');
 
 const app = express();
 dotenv.config();
 
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname,'access.log'), 
+  {flags: 'a'}
+);
+
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(courseRoutes);
+
+app.use(helmet());
+app.use(morgan('combined', {stream: accessLogStream}));
 
 mongoose
   .connect(process.env.MONGO_URI)
