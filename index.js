7c39ed11c5251a74bb95cd6a26547112e99d619f
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-30 03:50:48 +0530

    Adding CORS header to allow access from anywhere, POST & GET methods and to allow headers

--- index.js
+++ index.js
@@ -21,6 +21,16 @@ const accessLogStream = fs.createWriteStream(
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(courseRoutes);
 
 app.use(helmet());
