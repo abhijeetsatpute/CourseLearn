4c40c96291f51d2c845f96215a9aae8e0576c79b
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-29 21:34:19 +0530

    mongoDB database connection with mongoose ODM

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const courseRoutes = require('./routes/course');
 
 const app = express();
+dotenv.config();
 
 app.use(courseRoutes);
 
-app.listen(80, function () {
-    console.log('🎇 App listening on port 80');
-})
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(result => {
+    console.log("🎉 Database connected.");
+    app.listen(process.env.PORT || 80, function () {
+        console.log('✨ App listening on port', this.address().port);
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
